67464d2a7831ec3a548556b7dc8bca1c41810b95
aminakm123 <shamrifari@gmail.com>
2024-01-17 10:54:10 +0530

    login page modified

--- user/forms.py
+++ user/forms.py
@@ -1,5 +1,23 @@
 from django import forms
+from django.forms.widgets import TextInput
 
 class LoginForm(forms.Form):
-    username = forms.CharField(label='Username', max_length=100)
-    password = forms.CharField(label='Password', widget=forms.PasswordInput())
+    username = forms.CharField(
+        label='Username',
+        max_length=100,
+        widget=TextInput(attrs={'class': 'required form-control', 'placeholder': 'Enter your username'})
+    )
+    password = forms.CharField(
+        label='Password',
+        widget=forms.PasswordInput(attrs={'class': 'required form-control', 'placeholder': 'Enter your password'})
+    )
+
+
+# class LoginForm(forms.Form):
+#     username = forms.CharField(label='Username', max_length=100)
+#     password = forms.CharField(label='Password', widget=forms.PasswordInput())
+#     class Meta:
+#         widgets = {            
+#                 'username': TextInput(attrs={'class': 'required form-control', 'placeholder': 'Enter your username'}),
+#                 'password': TextInput(attrs={'class': 'required form-control', 'placeholder': 'Enter your password'})            
+#             }
