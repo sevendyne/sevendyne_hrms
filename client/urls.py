c8b20a35bdb4333eca9b4dfcff3be5fd2304c9e7
aminakm123 <shamrifari@gmail.com>
2024-05-21 14:33:20 +0530

    clients to projects

--- client/urls.py
+++ client/urls.py
@@ -2,11 +2,11 @@ from django.urls import path, re_path
 from client import views
 
 urlpatterns = [    
-    path('client/create/', views.create_client, name='create_client'),
-    path("clients/", views.clients, name="clients"),
-    path("clients-list/", views.clients_list, name="clients_list"),
-    re_path(r'^client/edit/(?P<pk>.*)/$', views.edit_client, name='edit_client'),
-    re_path(r'^delete-client/(?P<pk>.*)/$', views.delete_client, name='delete_client'),    
-    re_path(r'^client/(?P<pk>.*)/$', views.client, name='client')        
+    path('project/create/', views.create_client, name='create_client'),
+    path("projects/", views.clients, name="clients"),
+    path("projects-list/", views.clients_list, name="clients_list"),
+    re_path(r'^project/edit/(?P<pk>.*)/$', views.edit_client, name='edit_client'),
+    re_path(r'^delete-project/(?P<pk>.*)/$', views.delete_client, name='delete_client'),    
+    re_path(r'^project/(?P<pk>.*)/$', views.client, name='client')        
 ]
 
