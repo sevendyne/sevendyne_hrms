1cef6bf464be0425c3c962736428ef13debe636c
aminakm123 <shamrifari@gmail.com>
2024-06-11 14:28:24 +0530

    db from sqlite to postgres

--- sevendyne_hrms/settings.py
+++ sevendyne_hrms/settings.py
@@ -97,12 +97,24 @@ WSGI_APPLICATION = 'sevendyne_hrms.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'sevendyne_hrms', 
+        'USER': 'sevendyne',
+        'PASSWORD': 'sevendyne@123', 
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
 DBBACKUP_STORAGE = 'django.core.files.storage.FileSystemStorage'
 DBBACKUP_STORAGE_OPTIONS = {'location': BASE_DIR / 'backup/'}
 
