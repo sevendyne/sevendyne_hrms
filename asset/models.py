709748b75844a8547e824b9f8e43c3a1430c6cca
aminakm123 <shamrifari@gmail.com>
2024-07-09 11:55:35 +0530

    asset management

---
+++ asset/models.py
@@ -0,0 +1,56 @@
+from decimal import Decimal
+from django.db import models
+from django.utils import timezone
+from datetime import timedelta
+from main.models import BaseModel
+from django.utils.translation import gettext_lazy as _
+
+
+class Asset(BaseModel):
+    company = models.ForeignKey("main.Company",on_delete=models.CASCADE,limit_choices_to={'is_deleted': False})
+    name = models.CharField(_("Asset Name"),max_length=255)
+    description = models.TextField(_("Asset Description"),null=True,blank=True)
+    purchase_date = models.DateField(_("Purchase Date"))
+    purchase_price = models.DecimalField(_("Purchase Price"),max_digits=10, decimal_places=2)
+    useful_life_years = models.PositiveIntegerField(_("Useful Life in Years [After how many years will this asset expire?]"))
+    salvage_value = models.DecimalField(_("Salvage/Scrap Value"),max_digits=10, decimal_places=2) 
+    bill = models.FileField(_("Upload Purchase Bill"),null=True,blank=True)
+    photo = models.ImageField(_("Images of Asset "),upload_to='assets/images/', null=True, blank=True)
+    is_deleted = models.BooleanField(_('Is This Asset Deleted ?'),default=False)
+
+    class Meta:
+        db_table = ('asset_asset')
+        verbose_name = _('Asset')
+        verbose_name_plural = _('Assets')
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def depreciation_amount(self):
+        return (self.purchase_price - self.salvage_value) / self.useful_life_years
+
+    @property
+    def accumulated_depreciation(self):
+        years_elapsed = Decimal((timezone.now().date() - self.purchase_date).days) / Decimal(365.25)
+        accumulated_depreciation = min(self.purchase_price - self.salvage_value, years_elapsed * self.depreciation_amount)
+        return accumulated_depreciation.quantize(Decimal('0.01'))
+
+
+    @property
+    def current_value(self):
+        value = self.purchase_price - self.accumulated_depreciation
+        return value.quantize(Decimal('0.01'))
+
+# class AssetMaintenance(BaseModel):
+#     asset = models.ForeignKey(Asset, on_delete=models.CASCADE, related_name='maintenance_records')
+#     maintenance_date = models.DateField()
+#     cost = models.DecimalField(max_digits=10, decimal_places=2)
+#     description = models.TextField()
+#     bill = models.FileField()
+
+#     def __str__(self):
+#         return f"Maintenance {self.id} for {self.asset.name}"
+    
+    
